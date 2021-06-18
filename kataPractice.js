9713780f79e8dd8c70c1d5bea4954ff85faec027
David Bueno <davidbuenox8@gmail.com>
2021-06-18 12:47:29 +0200

    kata 18.06

--- kataPractice.js
+++ kataPractice.js
@@ -80,3 +80,23 @@ function i(word) {
     }
     return 'Invalid word'
 }
+
+/* Write a function that takes a string of parentheses, and determines
+ if the order of the parentheses is valid. The function
+ should return true if the string is valid, and false if it's invalid. */
+
+function validParentheses(parens) {
+    let count = 0
+    for (let char of parens) {
+        if (char === '(') {
+            count++
+        }
+        if (char === ')') {
+            count--
+        }
+        if (count < 0) {
+            return false
+        }
+    }
+    return count === 0
+}
