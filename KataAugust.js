bc0e51fd95d702a39ecde90e9f58bfc1a4117f13
David Bueno <davidbuenox8@gmail.com>
2021-08-25 13:30:34 +0200

    kata 25.08.2021

--- KataAugust.js
+++ KataAugust.js
@@ -10,3 +10,42 @@ function humanReadable(seconds) {
 
   return `${hh}:${mm}:${ss}`;
 }
+
+// get the first two sum pairs and make it optimal
+function sumPairs(ints, s) {
+  let seen = {};
+  for (let num of ints) {
+    if (seen[s - num]) return [s - num, num];
+    seen[num] = true;
+    console.log(seen);
+  }
+}
+
+//non-optimal
+function sumPairs(ints, s) {
+  let pair = undefined;
+  let length = ints.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      if (ints[i] + ints[j] == s) {
+        pair = [ints[i], ints[j]];
+        length = j;
+      }
+    }
+  }
+  return pair;
+}
+
+//and first attempt
+function sumPairs(ints, s) {
+  let pairs = {};
+  for (let i = 0; i < ints.length; i++) {
+    for (let j = i + 1; j < ints.length; j++) {
+      if (ints[i] + ints[j] === s) {
+        pairs[j] = [ints[i], ints[j]];
+      }
+    }
+  }
+  let pairsKeys = Math.min(parseInt(Object.keys(pairs)));
+  return pairs[pairsKeys] || undefined;
+}
