cc449be19d7a896ad1b3c82dc48a71f972379e1e
David Bueno <davidbuenox8@gmail.com>
2021-11-17 16:38:59 +0100

    another kata

--- KataSeptember.js
+++ KataSeptember.js
@@ -83,3 +83,23 @@ function maxSequence(arr) {
   }
   return maxSum;
 }
+
+//Find the Smallest (insanely difficult)
+
+Array.prototype.move = function (from, to) {
+  this.splice(to, 0, this.splice(from, 1)[0]);
+  return this;
+};
+
+function smallest(n) {
+  let iter = `${n}`.length,
+    res = new Map();
+  for (let i = 0; i < iter; i++) {
+    for (let j = 0; j < iter; j++) {
+      let number = `${n}`.split('').move(i, j).join('');
+      if (!res.has(+number)) res.set(+number, [i, j]);
+    }
+  }
+  let min = Math.min(...res.keys());
+  return [min, ...res.get(min)];
+}
